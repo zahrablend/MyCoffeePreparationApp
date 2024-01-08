8d2fc7b72811bc4d7415606118802a064e9748b5
zahrablend <zahra@LAPTOP-010BK67M>
2024-01-08 11:25:37 +0200

    Add FielAccess classes to distinguish between Json and Xml

---
+++ MyCoffeePreparationApp/FileAccess/StringsXmlRepository.cs
@@ -0,0 +1,25 @@
+﻿using MyCoffeePreparationApp.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MyCoffeePreparationApp.FileAccess
+{
+    public class StringsXmlRepository : StringsRepository
+    {
+        protected override string StringsToText(List<string> data)
+        {
+            var xmlDoc = new XDocument(new XElement("data", data.Select(s => new XElement("string", s))));
+            return xmlDoc.ToString();
+        }
+
+        protected override List<string> TextToStrings(string fileContents)
+        {
+            var xmlDoc = XDocument.Parse(fileContents);
+            return xmlDoc.Root.Elements("string").Select(e => e.Value).ToList();
+        }
+    }
+}
--- MyCoffeePreparationApp/Program.cs
+++ MyCoffeePreparationApp/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿/*
+ Use StringsXmlRepository class to read and write in xml formal
+ var repo = new StringsXmlRepository();
+ var strings = new List<string> { "foo", "bar", "baz" };
+ repo.Write("strings.xml", strings);
+ var readStrings = repo.Read("strings.xml");
+ */
